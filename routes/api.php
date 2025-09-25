67e4fb898c5616a94d48106315540bb3887a0f8d
Tohadi <toadiasley243@gmail.com>
2025-09-25 16:45:22 +0700

    route api

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Kalibrasi\KalibrasiController;
+use App\Http\Controllers\Kalibrasi\KalibrasiPressureController;
+
+Route::prefix('kalibrasi')->group(function () {
+    Route::get('/data/master/alat', [KalibrasiController::class, 'getDataAlatKalibrasi']);
+    Route::get('/show/master/alat/{id}', [KalibrasiController::class, 'showAlatKalibrasi']);
+    Route::get('/master/filters', [KalibrasiController::class, 'getFilters']);
+
+    Route::prefix('pressure')->group(function () {
+        Route::post('/store', [KalibrasiPressureController::class, 'store']);
+        Route::get('/data/alat/{id}', [KalibrasiPressureController::class, 'show']);
+    });
+});
