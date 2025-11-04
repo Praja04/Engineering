41823ae98202c63d1bda07d62ac71d43fbc42eab
Tohadi <toadiasley243@gmail.com>
2025-11-04 14:01:44 +0700

    Refactor sidebar boiler dashboard: enhance structure and add collapsible menu for better navigation

--- resources/views/layouts/components/sidebar-boiler/dashboard.blade.php
+++ resources/views/layouts/components/sidebar-boiler/dashboard.blade.php
@@ -5,9 +5,24 @@
 
 @if ($jabatan != 'operator' && $bagian == 'Engineering')
     <li class="nav-item">
-        <a href="{{ route('boiler.dashboard') }}"
-            class="nav-link menu-link {{ request()->routeIs('boiler.dashboard') ? 'active' : '' }}">
-            <i class="mdi mdi-cog"></i> <span data-key="t-albras">Dashboard Boiler</span>
+        <a class="nav-link menu-link" href="#sidebarDashboardBoiler" data-bs-toggle="collapse" role="button"
+            aria-expanded="false" aria-controls="sidebarDashboardBoiler">
+            <i class="mdi mdi-cog"></i>
+            <span data-key="t-dashboards">Dashboard Boiler</span>
         </a>
+        <div class="collapse menu-dropdown" id="sidebarDashboardBoiler">
+            <ul class="nav nav-sm flex-column">
+                <li class="nav-item">
+                    <a href="{{ route('boiler.dashboard') }}" class="nav-link" data-key="t-analytics">
+                        <i class="mdi mdi-chart-line me-2"></i> Dashboard Realtime
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ url('dashboard/mesin/downtime') }}" class="nav-link" data-key="t-analytics">
+                        <i class="mdi mdi-bullseye-arrow me-2"></i> Dashboard KPI
+                    </a>
+                </li>
+            </ul>
+        </div>
     </li>
 @endif
