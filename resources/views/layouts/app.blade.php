7c7bce7bb66d80108772e0944f80992271fdbb68
Tohadi <toadiasley243@gmail.com>
2025-11-04 14:00:44 +0700

    Update page title to include dynamic section for better context

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Engineering</title>
+        <title>Engineering | @yield('title')</title>
         <meta content="Themesbrand" name="author" />
 
         <script>
