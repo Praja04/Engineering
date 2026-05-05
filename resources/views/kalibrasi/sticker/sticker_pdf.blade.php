3cc505951ade4955c943d5222fe7bd0459d8d7dd
Tohadi <toadiasley243@gmail.com>
2026-05-05 09:21:47 +0700

    feat: redesign sticker kalibrasi

--- resources/views/kalibrasi/sticker/sticker_pdf.blade.php
+++ resources/views/kalibrasi/sticker/sticker_pdf.blade.php
@@ -6,74 +6,162 @@
     <style>
         @page {
             margin: 0;
+            size: 283.46pt 113.38pt;
+            /* Explicitly define size in CSS too */
         }
 
+        * {
+            box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+        }
+
+        html,
         body {
             margin: 0;
-            padding: 6px;
-            font-family: Arial, sans-serif;
+            padding: 0;
+            width: 283.46pt;
+            height: 113.38pt;
+            overflow: hidden;
+            font-family: 'Helvetica', 'Arial', sans-serif;
+            background-color: #fff;
+        }
+
+        .sticker-box {
+            width: 283.46pt;
+            height: 113.38pt;
+            border: 2pt solid #2c3e50;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background-color: #fff;
+            overflow: hidden;
+        }
+
+        .header {
+            background-color: #2c3e50;
+            padding: 4pt 10pt;
+            color: white;
+            height: 28pt;
+            width: 100%;
+        }
+
+        .logo-container {
+            float: left;
+            width: 80pt;
+            height: 20pt;
+        }
+
+        .logo-container img {
+            max-height: 20pt;
+            max-width: 80pt;
+        }
+
+        .header-text {
+            float: right;
+            text-align: right;
+            padding-right: 20pt;
+            font-weight: bold;
+            font-size: 11pt;
+            margin-top: 2pt;
+        }
+
+        .content {
+            padding: 5pt 10pt;
+            clear: both;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 2pt;
+        }
+
+        td {
+            padding: 2pt 0;
+            vertical-align: middle;
+            border-bottom: 1px solid #f0f0f0;
             font-size: 10px;
         }
 
-        .wrapper {
-            border: 2px solid #000;
-            padding: 6px;
-        }
-
-        .logo {
-            text-align: center;
-            margin-bottom: 4px;
-        }
-
-        .logo img {
-            height: 20px;
-        }
-
-        .row {
-            margin-bottom: 3px;
-        }
-
         .label {
+            font-weight: bold;
+            color: #555;
+            width: 90pt;
+            text-transform: uppercase;
+        }
+
+        .value {
+            color: #000;
+            font-weight: bold;
+            font-size: 11px;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 3pt;
+            right: 10pt;
+            font-size: 7px;
+            color: #7f8c8d;
+            font-style: italic;
+        }
+
+        .highlight {
+            color: #c0392b;
             font-weight: bold;
         }
     </style>
 </head>
 
 <body>
+    @php
+    $path = public_path('assets/images/logo/bas.png');
+    $base64 = null;
+    if (file_exists($path)) {
+    $type = pathinfo($path, PATHINFO_EXTENSION);
+    $data = file_get_contents($path);
+    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+    @endphp
 
-    <div class="wrapper">
-
-        <div class="logo">
-            <img src="{{ public_path('assets/images/logo/bas.png') }}">
+    <div class="sticker-box">
+        <div class="header">
+            <div class="logo-container">
+                @if($base64)
+                <img src="{{ $base64 }}">
+                @else
+                <span style="font-size: 9pt; font-weight: bold;">PT. BAS</span>
+                @endif
+            </div>
+            <div class="header-text">
+                STIKER KALIBRASI
+            </div>
         </div>
 
-        <div class="row">
-            <span class="label">Kode:</span>
-            {{ $kalibrasi->alat->kode_alat ?? '-' }}
+        <div class="content">
+            <table>
+                <tr>
+                    <td class="label">ID Alat</td>
+                    <td class="value">: {{ $kalibrasi->alat->kode_alat ?? '-' }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Tgl Kalibrasi</td>
+                    <td class="value">: {{ \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi)->format('d-m-Y') }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Tgl Kalib. Ulang</td>
+                    <td class="value">: <span class="highlight">{{ $kalibrasi->tgl_kalibrasi_ulang ? \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi_ulang)->format('d-m-Y') : \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi)->addYear()->format('d-m-Y') }}</span></td>
+                </tr>
+                <tr>
+                    <td class="label">Koreksi</td>
+                    <td class="value">: {{ number_format($kalibrasi->getMaxKoreksi(), 3) }}</td>
+                </tr>
+            </table>
         </div>
 
-        <div class="row">
-            <span class="label">Dibuat:</span>
-            {{ $kalibrasi->user->username ?? '-' }}
+        <div class="footer">
+            Oleh: {{ $kalibrasi->user->username ?? '-' }} | Generated: {{ now()->format('d/m/Y') }}
         </div>
-
-        <div class="row">
-            <span class="label">Kalibrasi:</span>
-            {{ \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi)->format('d-m-Y') }}
-        </div>
-
-        <div class="row">
-            <span class="label">Ulang:</span>
-            {{ $kalibrasi->tgl_kalibrasi_ulang ? \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi_ulang)->format('d-m-Y') : \Carbon\Carbon::parse($kalibrasi->tgl_kalibrasi)->addYear()->format('d-m-Y') }}
-        </div>
-
-        <div class="row">
-            <span class="label">Koreksi:</span>
-            {{ number_format($kalibrasi->getMaxKoreksi(), 3) }}
-        </div>
-
     </div>
-
 </body>
 
 </html>
